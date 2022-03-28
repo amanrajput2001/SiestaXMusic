a3c468b43cd7d5199980465b58fe6b1597a05bf7
amanrajput2001 <90300203+amanrajput2001@users.noreply.github.com>
2022-03-28 18:55:27 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -64,7 +64,7 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
-SUPPORT_Channel = "https://t.me/itsamanrajput"
+SUPPORT_CHANNEL = "https://t.me/itsamanrajput"
 SUPPORT_GROUP = "https://t.me/Friends_Chatting_Group3"
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
